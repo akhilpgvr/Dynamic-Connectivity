a5775c3ad08a2dbb21f28253dcd5d9fb59757278
Akhil <akhilpgvr@gmail.com>
2024-07-17 10:47:08 +0530

    added Dynamic connectivity concept using trees

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,12 @@ public class Main {
 
         //Using Arrays
         System.out.println("..................................");
+        System.out.println("................Using Array Concept..................");
+        System.out.println("..................................");
         ArrayConcept arrConnec = new ArrayConcept();
+        System.out.print("Enter the number of vertices: ");
         int verticesCount = Integer.parseInt(bufferedReader.readLine().trim());
         arrConnec.quickFind(verticesCount);
-        System.out.println("..................................");
         System.out.print("Enter the number of unions to perform: ");
         int unionCount = Integer.parseInt(bufferedReader.readLine().trim());
         for(int i=0; i<unionCount; i++) {
@@ -30,6 +32,29 @@ public class Main {
         }
         System.out.println("..................................");
 
+
+        //Using Trees
+        System.out.println("..................................");
+        System.out.println("................Using Tree Concept..................");
+        System.out.println("..................................");
+        TreeConcept treeConnec = new TreeConcept();
+        System.out.print("Enter the number of vertices: ");
+        verticesCount = Integer.parseInt(bufferedReader.readLine().trim());
+        treeConnec.quickFind(verticesCount);
+        System.out.print("Enter the number of unions to perform: ");
+        unionCount = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i=0; i<unionCount; i++) {
+            List<Integer> vertices = Arrays.stream(bufferedReader.readLine().trim().split(" ")).map(Integer::parseInt).toList();
+            treeConnec.union(vertices.get(0), vertices.get(1));
+        }
+        System.out.println("..................................");
+        System.out.print("Enter the number of connection check to perform: ");
+        connectionCheckCount = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i=0; i<connectionCheckCount; i++) {
+            List<Integer> vertices = Arrays.stream(bufferedReader.readLine().trim().split(" ")).map(Integer::parseInt).toList();
+            treeConnec.connected(vertices.get(0), vertices.get(1));
+        }
+        System.out.println("..................................");
         bufferedReader.close();
     }
 }
---
+++ src/TreeConcept.java
@@ -0,0 +1,28 @@
+public class TreeConcept {
+
+    int[] id = new int[10];
+
+    public void quickFind(int size) {
+
+        for(int i=0; i<size; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int root(int p) {
+
+        while(id[p]!=p) p=id[p];
+        return p;
+    }
+
+    public void union(int p, int q) {
+
+        id[root(p)] = id[root(q)];
+    }
+
+    public void connected(int p, int q) {
+
+        if(root(p) == root(q)) System.out.println("Connected");
+        else System.out.println("Not connected");
+    }
+}
